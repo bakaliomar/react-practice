9a95eb9f8da008cf38eadfdb3f4ad2649fe02446
bakaliomar <elbakaliomar9@gmail.com>
2022-10-07 19:10:21 +0100

    chore: some basics

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+//import ProjectsPage from './projects/ProjectsPage';
+//import Hello from './Helo';
+import HelloSecond from "./HelloSecond";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <HelloSecond name="Susan" enthusiasmLevel={8} />
     </div>
   );
 }
